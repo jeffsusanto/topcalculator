f81afb113e159d0476ec830e8c4f949f374779bb
jeffsusanto <susantojeffy@gmail.com>
2022-07-23 09:50:51 +0800

    created index.html, style.css, app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+const add = function (num1, num2) {
+    return num1 + num2;
+};
+
+const subtract = function (num1, num2) {
+    return num1 - num2;
+};
+
+const multiply = function(num1,num2) {
+    return num1 * num2;
+}
+
+const divide = function(num1,num2) {
+    return num1 / num2;
+}
+
+
+
+// const sum = function (array) {
+//     let sum = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         sum += array[i];
+//     }
+//     return sum;
+// };
+
+// const multiplyArray = function (array) {
+//     return array.length
+//         ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+//         : 0;
+// };
